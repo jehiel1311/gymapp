27a2f0922f2ece4badadfc01cbde5bc4c1dbb5f4
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-23 21:52:33 -0300

    Update vite config base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ¡IMPORTANTE! Esto es para deploy en GitHub Pages en /gymapp/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',
+  base: '/gymapp/',
 })
