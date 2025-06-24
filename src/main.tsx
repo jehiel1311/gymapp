ac235fa9a6c1f5d9b2610bf74337667d4506e018
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-24 20:44:54 -0300

    Wrap App with ExerciseProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
+import { ExerciseProvider } from "./context/ExerciseContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/gymapp">
-      <App />
+      <ExerciseProvider>
+        <App />
+      </ExerciseProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
