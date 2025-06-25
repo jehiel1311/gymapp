0c8955df8210528c99c941524c923de44c268fd2
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-24 21:39:34 -0300

    Update App routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,13 @@
-import { Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
 import Home from "./pages/Home";
-import ExerciseDetail from "./pages/ExerciseDetail";
-import RoutineBuilder from "./pages/RoutineBuilder";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
-        <Route path="/rutina" element={<RoutineBuilder />} />
-      </Routes>
-      <footer
-        style={{
-          position: "fixed",
-          bottom: 0,
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-around",
-          padding: ".5rem 0",
-          background: "#fff",
-          boxShadow: "0 -1px 4px rgba(0,0,0,.1)",
-        }}
-      >
-        <NavLink to="/">🏠 Inicio</NavLink>
-        <NavLink to="/rutina">📋 Rutina</NavLink>
-      </footer>
-    </>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 }
 
