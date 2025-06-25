db9105f8641ff602cdd1d8a6a747ca751287ffc2
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-24 21:03:31 -0300

    Add RoutineBuilder route and bottom navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
+import RoutineBuilder from "./pages/RoutineBuilder";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="/rutina" element={<RoutineBuilder />} />
+      </Routes>
+      <footer
+        style={{
+          position: "fixed",
+          bottom: 0,
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-around",
+          padding: ".5rem 0",
+          background: "#fff",
+          boxShadow: "0 -1px 4px rgba(0,0,0,.1)",
+        }}
+      >
+        <NavLink to="/">🏠 Inicio</NavLink>
+        <NavLink to="/rutina">📋 Rutina</NavLink>
+      </footer>
+    </>
   );
 }
 
