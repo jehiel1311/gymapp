2dfaf23f5d472a78c8c3f25317d80c99e9df163b
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-24 21:45:36 -0300

    feat: exercise listing and routine page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import RoutineBuilder from "./pages/RoutineBuilder";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      <Route path="/routine" element={<RoutineBuilder />} />
     </Routes>
   );
 }
