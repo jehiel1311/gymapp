c8e0a3c6a9bd7861835c796cf725f4f634791fd9
jehiel1311 <136872868+jehiel1311@users.noreply.github.com>
2025-06-24 21:58:17 -0300

    Remove ExerciseProvider wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
-import { ExerciseProvider } from "./context/ExerciseContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/gymapp">
-      <ExerciseProvider>
-        <App />
-      </ExerciseProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
